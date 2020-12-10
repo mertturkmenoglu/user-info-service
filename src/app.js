ac3f1c5082aba461aaf8d6c59f1718634fee1689
Mert <mertturkmenoglu99@gmail.com>
2020-12-10 16:08:10 +0300

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,22 +6,38 @@ const cors = require('cors')
 
 const userRoutes = require('./routes/userRoutes')
 
-
-const app = express();
-dotenv.config();
-
-app.use(express.json());
+const app = express()
+dotenv.config()
+app.use(express.json())
 app.use(morgan('[:date[web]] || :method :url  || Status: :status || Response time: :response-time ms'))
 app.use(cors())
 
-const MONGOOSE_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true };
-mongoose.connect(process.env.DB_CONNECTION, MONGOOSE_OPTIONS, () => {
-	console.log('Connected to MongoDB database');
-});
+const MONGOOSE_OPTIONS = { useNewUrlParser: true, useUnifiedTopology: true }
+const DB = process.env.NODE_ENV  === 'production'
+	? process.env.DB_CONNECTION
+	: process.env.DB_TEST_CONNECTION
 
+mongoose.connect(DB, MONGOOSE_OPTIONS, () => {
+	console.log('Connected to MongoDB database')
+	console.log(`Node environment: ${process.env.NODE_ENV}`)
+})
 
-app.use('/user', userRoutes);
+app.get('/', (req, res) => {
+	return res.json({
+		message: 'Vevericka User Info Service'
+	})
+})
 
+app.use('/user', userRoutes)
 
-const PORT = process.env.PORT || 3232;
-app.listen(PORT, () => console.log(`Server started on ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.use('*', (req, res, next) => {
+	res.status(404).send('Not Found')
+	next()
+})
+
+const PORT = process.env.PORT || 3232
+const server = app.listen(PORT, () => {
+	console.log(`Server started on ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
+
+module.exports = server
